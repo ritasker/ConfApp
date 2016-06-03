4947c8df520e46759235b2838b540ed25cd94be5
Rich Tasker <ritasker8t2@gmail.com>
2016-06-03 19:48:50 +0100

    Adds bootstrap and a home controller

---
+++ ConfApp.Tests/Controller/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using ConfApp.Web.Controllers;
+using Xunit;
+using FluentAssertions;
+
+namespace ConfApp.Tests.Controller
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ShouldReturnAView()
+        {
+            var subject = new HomeController();
+
+            var result = subject.Index();
+
+            result.Should().NotBeNull();
+        }
+    }
+}

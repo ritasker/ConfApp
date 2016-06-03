6f7e1b8de184b264c9a3bdbad9ed3d52a54f309c
Rich Tasker <ritasker8t2@gmail.com>
2016-06-03 19:49:27 +0100

    Adds a conference controller

---
+++ ConfApp.Tests/Controller/ConferenceControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using ConfApp.Web.Controllers;
+using FluentAssertions;
+using Xunit;
+
+namespace ConfApp.Tests.Controller
+{
+    public class ConferenceControllerTests
+    {
+        [Fact]
+        public void Index_ShouldReturnAListOfConferences()
+        {
+            var subject = new ConferenceController();
+            var result = subject.Index();
+
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ ConfApp.Web/Controllers/ConferenceController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ConfApp.Web.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
